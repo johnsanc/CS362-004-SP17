f085755bff19e6147bf6179bb8b3308625aea29c
Johnny Sanchez <sanchjoh@oregonstate.edu>
2017-05-14 22:53:53 -0700

    Edit to previous commit, completed and tested Assignment 4.

---
+++ projects/sanchjoh/dominion/randomtestadventurer.c
@@ -0,0 +1,98 @@
+#include "dominion.h"
+#include "dominion_helpers.h"
+#include <string.h>
+#include <stdio.h>
+#include <assert.h>
+#include <math.h>
+#include "rngs.h"
+
+
+int checkAdventurerCard(int p, struct gameState *post) {
+  int drawCorrect, drawIncorrect, test1, test2, discardBefore,
+      discardAfter, deckBefore, deckAfter, handBefore, handAfter, totalBefore,
+      totalAfter, checkCard;
+
+  // Need to keep track of the amount in discard pile, deck size, hand size to
+  // make sure amount are correct.  Hand will need to have +2 before function is
+  // called because other cards are destroyed and 1 treasure cards are kept in
+  // hand.
+  handBefore = post->handCount[p];
+  deckBefore = post->deckCount[p];
+  discardBefore = post->discardCount[p];
+
+  // Run the function
+  checkCard = playAdventurer(post);
+  drawCorrect = post->hand[p][post->handCount[p] - 1];
+  drawIncorrect = post->hand[p][post->handCount[p]];
+
+  assert (checkCard == 0);
+  // Get Results
+  handAfter = post->handCount[p];
+  deckAfter = post->deckCount[p];
+  discardAfter = post->discardCount[p];
+
+  // Run the totals
+  totalBefore = handBefore + deckBefore + discardBefore;
+  totalAfter = handAfter + deckAfter + discardAfter;
+
+  // Testing if correct card was selected for cardDrawn
+  if (drawIncorrect != drawCorrect) {
+    test1 = 0;
+  } else test1 = 1;
+
+  // Testing if the totals for the cards in use had changed.
+  if (totalBefore == totalAfter) {
+    test2 = 1;
+  } else test2 = 0;
+
+  // Results of TESTS
+  if (test1 == 1 && test2 == 1) {
+    return 1;
+  } else if ((test1 == 0 && test2 == 1) || (test1 == 1 && test2 == 0)) {
+    return -1;
+  } else return 0;
+
+}
+
+int main() {
+
+  int p, n, i, result;
+  struct gameState G;
+
+  int k[10] = {adventurer, council_room, feast, gardens, mine,
+	       remodel, smithy, village, baron, great_hall};
+
+  // printf("Testing playAdventurer.\n");
+  //
+  // printf("Testing if correct card is drawn.\n");
+
+  // Initiate ramdomizer
+  SelectStream(2);
+  PutSeed(3);
+
+  for (n = 0; n < 10000; n++) {
+
+    // Fill game post with random stuff.
+    for (i = 0; i < sizeof(struct gameState); i++) {
+      ((char*)&G)[i] = floor(Random() * 256);
+    }
+
+    // Fill game post with credible/in-range values
+    // Since the Adventurer card relies on the ability to draw cards, we need
+    // These values to stay within the allowabled ranges to accurately test.
+    p = floor(Random() * 2);
+    initializeGame(2, k, 1, &G);
+    G.deckCount[p] = floor(Random() * MAX_DECK);
+    G.discardCount[p] = floor(Random() * MAX_DECK);
+    G.handCount[p] = floor(Random() * MAX_HAND);
+
+    result = checkAdventurerCard(p, &G);
+  }
+  if (result == 1) {
+    printf("ALL TESTS PASSED\n");
+  } else if (result == -1) {
+    printf("1 TEST PASSED OUT OF 2\n");
+  } else {
+    printf("ALL TESTS FAILED\n");
+  }
+}
---
+++ projects/sanchjoh/dominion/randomtestcard2.c
@@ -0,0 +1,84 @@
+#include "dominion.h"
+#include "dominion_helpers.h"
+#include <string.h>
+#include <stdio.h>
+#include <assert.h>
+#include <math.h>
+#include "rngs.h"
+
+int checkCouncil_Room(struct gameState *state, int p) {
+  struct gameState correct;
+  memcpy(&correct, state, sizeof(struct gameState));
+
+  int result;
+
+  result = playCouncil_Room(state, 0);
+
+
+  if (correct.deckCount[p] > 0) {
+    int counter = 0;
+    // Simulate drawing 4 cards
+    while (correct.deckCount[p] > 0 && counter < 4) {
+      correct.deckCount[p]--;
+      correct.handCount[p]++;
+      counter++;
+    }
+    // Simulate discarding a card from hand
+    correct.handCount[p]--;
+    correct.discardCount[p]++;
+  }
+
+  // Simulate updating number of buys
+  correct.numBuys++;
+
+  if (correct.deckCount[p] < state->deckCount[p]){
+    // printf("TEST1 FAILED\n");
+    return 0;
+  }
+  if (correct.numBuys > state->numBuys) {
+    // printf("TEST2 FAILED\n");
+    return 0;
+  }
+  if (correct.handCount[p] > state->handCount[p]) {
+    // printf("TEST3 FAILED\n");
+    return 0;
+  }
+
+  return 1;
+}
+
+int main() {
+  int p, n, i, result;
+  struct gameState G;
+
+  int k[10] = {adventurer, council_room, feast, gardens, mine,
+	       remodel, smithy, village, baron, great_hall};
+
+  // Initiate ramdomizer
+  SelectStream(2);
+  PutSeed(3);
+
+  for (n = 0; n < 2000; n++) {
+
+    // Fill game post with random stuff.
+    for (i = 0; i < sizeof(struct gameState); i++) {
+      ((char*)&G)[i] = floor(Random() * 256);
+    }
+
+    // Fill game post with credible/in-range values
+    // Since the Adventurer card relies on the ability to draw cards, we need
+    // These values to stay within the allowabled ranges to accurately test.
+    p = floor(Random() * 2);
+    initializeGame(2, k, 1, &G);
+    G.deckCount[p] = floor(Random() * MAX_DECK);
+    G.discardCount[p] = floor(Random() * MAX_DECK);
+    G.handCount[p] = floor(Random() * MAX_HAND);
+    result = checkCouncil_Room(&G, 0);
+  }
+
+  if (result == 0) {
+    printf("COUNCIL ROOM FAILED\n");
+  } else {
+    printf("COUNCIL ROOM PASSED\n");
+  }
+}
